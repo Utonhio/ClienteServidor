761f6568381460be46cec82afcaddea8de63b9a6
Brandon <Utonhio@gmail.com>
2018-05-24 09:43:02 -0500

    Cree el archivo de nuevo

---
+++ app.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var path = require('path');
+var express = require('express');
+var logger=require('morgan');
+var bodyParser=require(body-parser);
+
+var app = express();
+app.set('views',path.resolve)
